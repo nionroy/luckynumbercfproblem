9cf195db7b222d35fc47b7b5daaea90300946c63
nionroy <136985474+nionroy@users.noreply.github.com>
2024-09-18 11:37:31 +0600

    Add files via upload

---
+++ cptamplate.cpp
@@ -0,0 +1,28 @@
+/*
+Nion roy
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIo ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl "\n"
+#define ll long long int
+
+void solve()
+{
+
+}
+
+
+int main()
+{
+
+    FastIo;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
